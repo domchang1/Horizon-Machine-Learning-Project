eee193b4ec8b9e98572c25a6c6ca53faffb980a7
domchang1 <dominicchang08@gmail.com>
2022-04-24 23:08:17 -0700

    first commit

---
+++ April 28th/main.py
@@ -0,0 +1,36 @@
+import numpy as np
+import imageio.v3 as iio
+import matplotlib.pyplot as plt
+
+def apply(kernel, image):
+    h_k = len(kernel)
+    w_k = len(kernel[0])
+    output = np.copy(image)
+    for y_i in range (len(image[0])):
+        for x_i in range (len(image[0][0])):
+            for dy in range(int(-(h_k-1)/2), int((h_k-1)/2) + 1):
+                for dx in range(int(-(w_k-1)/2),int((w_k-1)/2) + 1):
+                    for c in range(3):
+                        output[y_i, x_i] += image[y_i + dy][x_i + dx][c] * kernel[c][int((h_k-1)/2) + dy][int((w_k-1)/2)+ dx]
+    return output
+
+
+kernel =[[ [ -1, 0, 1 ],
+        [ -2, 0, 2 ],
+        [ -1, 0, 1 ]], [ [ -1, 0, 1 ],
+        [ -2, 0, 2 ],
+        [ -1, 0, 1 ]], [ [ -1, 0, 1 ],
+        [ -2, 0, 2 ],
+        [ -1, 0, 1 ]] ]
+kernel = np.array(kernel)
+print(kernel)
+cat = iio.imread('imageio:chelsea.png')
+print(cat.shape)
+img = np.array(cat)
+# print(img)
+#new = np.dot(img, kernel)
+new = apply(kernel, img)
+print (new)
+im = plt.imshow(new)
+
+
---
+++ April 28th/notes.md
@@ -0,0 +1,14 @@
+Goals:
+    -come up with formula to compute g from image directly
+    -given 2 kernels (1 for image to feature map f, 1 for f to g) 
+        -something about matrix of dot products of the matrices
+    -answer: why is relu necessary? Can a useful CNN be made up of only convolutions (no rectification)?
+        -relu increases the nonlinearity, is a lot faster, computationally inexpensive
+    -setup python venv w/ numpy, imageio; design kernels to detect something interesting, convolve some 
+    -images with those kernels (maybe numpy function for inner loop, dot & sum function), normalize brightness,
+    -try adding bias term, then using rectification, either save images or display using imshow in matplotlib
+
+ What was Acheived / Not:
+    -Got venv working, though have to use anaconda prompt (can't figure out otherwise)
+    -Got packages installed
+    -
