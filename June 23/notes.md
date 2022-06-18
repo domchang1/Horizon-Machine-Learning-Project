d7c8fd640c7dc81819e6741a0c2e8b79e7c83907
domchang1 <dominicchang08@gmail.com>
2022-06-17 23:39:31 -0700

    friday work

--- June 23/notes.md
+++ June 23/notes.md
@@ -1,10 +1,13 @@
 Goals:
     -look at preprocessing needed for imagenet, transforms.normalize
+        -https://github.com/pytorch/vision/issues/39 preprocessing
+        -
     -how to run pytorch module on image, look at images expected (transformations needed)
     -get the features for training and test set, put in folder with pickle files
     -scikit learn for some regression, predict diagnosis from features
 
  What was Acheived:
+    -Fri: preprocessing found, working on getting features to file
     -Monday:
     -Tues:
     -Wed:...
--- June 23/thing.py
+++ June 23/thing.py
@@ -4,16 +4,49 @@ from skimage.io import imread_collection
 from skimage.transform import resize
 import imageio.v3 as iio
 import numpy as np
-import torchvision
+from torchvision import transforms, models
 import torch
 from torchvision.utils import save_image
 import pandas as pd
 import pickle
+from PIL import Image
+import glob
 
 # load in images, resize and normalize, save each scenario (512 features + id + label) into new file
 
-# normalize = torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],
-#                                  std=[0.229, 0.224, 0.225])
+preprocessing = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225]),
+])
+resnet = models.resnet18(pretrained=True)
+newmodel = torch.nn.Sequential(*(list(resnet.children())[:-1]))
+# filename = '../test_images\\fef8e645d030.png'
+# id = filename[15:len(filename)-4]
+# print(id)
+
+image_list = {}
+for filename in glob.glob('../train_images/*.png'): 
+    im=Image.open(filename)
+    input = preprocessing(im)
+    input_batch = input.unsqueeze(0)
+    # if torch.cuda.is_available():
+    #     input = input_batch.to('cuda')
+    #     newmodel.to('cuda')
+    with torch.no_grad():
+        output = newmodel(input_batch)
+    id = filename[15:len(filename)-4]
+    image_list[filename] = output
+#print(image_list)
+
+#for i in l:
+    # new_image = transforms.ToPILImage(i)
+    # new_image.save(f"../new_test_images")
+
+
+
 # resnet = torchvision.models.resnet18(pretrained=True)
 # newmodel = torch.nn.Sequential(*(list(resnet.children())[:-1]))
 # device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -22,13 +55,11 @@ import pickle
 # print(newmodel)
 # # newmodel.train()
 # l = list(imread_collection("../train_images/*.png"))
-# for x in len(l):
-#     l[x]= resize(l[x], (256,256))
 training_set = pd.read_csv("../train.csv")
 #delimiter=",", skip_header=1, dtype=[("id_code", np.string_), ("diagnosis", int)]
 for i in range(len(training_set)):
     dst = Path(f"../trainingscenarios/{i:04d}.pkl")
-    dst.write_bytes(pickle.dumps(dict(id_code=training_set.id_code[i], diagnosis=training_set.diagnosis[i], features=None)))
+    dst.write_bytes(pickle.dumps(dict(id_code=training_set.id_code[i], diagnosis=training_set.diagnosis[i], features=image_list[training_set.id_code[i]])))
     # np.savez(f"../trainingscenarios/{i:04d}.npz", id_code=training_set.id_code[i], diagnosis=training_set.diagnosis[i], features=None)
     
 # for i in sorted(Path("../train_images").iterdir()):
